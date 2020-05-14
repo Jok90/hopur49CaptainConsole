9273a97dbb122d9e6e296f954af73ccda123d46d
kristjanleifur4 <54938706+kristjanleifur4@users.noreply.github.com>
2020-05-14 10:52:35 +0000

    moar wurk?

--- static/js/games.js
+++ static/js/games.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
             type: 'GET',
             success: function (resp) {
                 var newHtml = resp.data.map(d => {
-                    return '<div class="well-game">
+                    return `<div class="gamedescription">
                                 <a href="/store/${d.id}">
                                     <img class="game-img" src="${d.firstImage}" />
                                     <h4>${d.name}</h4>
                                     <p>${d.description}</p>
                                 </a>
-                            </div>'
+                            </div>`
                 });
                 $('.games').html(newHtml.join(''));
                 $('#search-box').val('');
